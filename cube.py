337c13e2b827a7b26b31e035d0159a89fe6789a4
Yannick Longval <yannicklongval2014@gmail.com>
2022-01-15 18:46:55 -0500

    almost done
    Added win screen, a few comments, fixed logistics for Cube.is_solved() method, made some code prettier.

--- cube.py
+++ cube.py
@@ -1,22 +1,27 @@
 import numpy as np
 import random
 
+#colour imports
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 ORANGE = (255, 165, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+
+#list of colour order (top, front, bottom, back, left, right)
 colours = [GREEN, WHITE, BLUE, YELLOW, RED, ORANGE]
 
 class Cube():
 
+    #creates solved rubiks cube
     def __init__(self):
         self.layout = np.empty((6, 3, 3, 3))
 
         for index, colour in enumerate(colours):
             self.layout[index] =  np.full((3, 3, 3), colour)
 
+    #changes values in array based on how the cube is turned
     def turn(self, dir):
         if dir == "LU":
             temp = np.copy(self.layout[0][0])
@@ -67,13 +72,22 @@ class Cube():
             self.layout[1][:, 0], self.layout[5][:, 0], self.layout[3][:, 2], self.layout[4][:, 0] = self.layout[5][:, 0], np.flip(self.layout[3][:, 2], 0), np.flip(self.layout[4][:, 0], 0), temp
             self.layout[0] = np.rot90(self.layout[0])
 
-    def shuffle(self, turns = 50):
+    #randomly does <turns> turns, essentially scrambling the cube
+    def scramble(self, turns = 50):
         moves = ['LU', 'LD', 'RU', 'RD', 'UR', 'UL', 'DR', 'DL', 'FR', 'FL', 'BR', 'BL']
         for i in range(turns):
             self.turn(random.choice(moves))
+
+    def compare_colours(self, s1, s2):
+        for i in range(3):
+            if s1[i] != s2[i]:
+                return False
+        return True
     
+    #Checks that all faces have a solid colour, signifying a solved cube
     def is_solved(self):
         for face in self.layout:
-            if face[0] != face[1] or face[1] != face[2]:
-                return False
+            for row in face:
+                if not self.compare_colours(row[0], row[1]) or not self.compare_colours(row[1], row[2]):
+                    return False
         return True
--- main.py
+++ main.py
@@ -6,14 +6,35 @@ from pygame.constants import MOUSEBUTTONDOWN
 from cube import Cube
 pygame.font.init()
 
+#button sizes
+ARROW_BUTTON_SIZE = (45, 45)
+
+#load in images
 ARROW_IMAGE = pygame.image.load(os.path.join('img/Arrow.png'))
 ARROW_TURN_IMAGE = pygame.image.load(os.path.join("img/Arrow_Turn.png"))
-ARROW_TURN = pygame.transform.scale(ARROW_TURN_IMAGE, (45, 45))
-ARROW_UP = pygame.transform.scale(ARROW_IMAGE, (45, 45))
+ARROW_TURN = pygame.transform.scale(ARROW_TURN_IMAGE, ARROW_BUTTON_SIZE)
+ARROW_UP = pygame.transform.scale(ARROW_IMAGE, ARROW_BUTTON_SIZE)
 ARROW_RIGHT = pygame.transform.rotate(ARROW_UP, 270)
 ARROW_LEFT = pygame.transform.rotate(ARROW_UP, 90)
 ARROW_DOWN = pygame.transform.rotate(ARROW_UP, 180)
 
+# Locations of arrows and action if pressed
+buttons = {
+    (229, 13): "LU",
+    (329, 13): "RU",
+    (229, 690): "LD",
+    (329, 690): "RD",
+    (19, 225): "BL",
+    (19, 325): "FL",
+    (539, 225): "BR",
+    (539, 325): "FR",
+    (384, 170): "UL",
+    (174, 170): "UR",
+    (384, 480): "DR",
+    (174, 480): "DL"
+}
+
+#font imports
 TEXT_FONT = pygame.font.SysFont('comicsans', 80)
 SMALL_TEXT = pygame.font.SysFont('comicsans', 40)
 
@@ -35,6 +56,15 @@ def draw_pause_window():
     WIN.fill(BACKGROUND)
     pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 190, HEIGHT // 2 - 225, 400, 100))
     WIN.blit(TEXT_FONT.render('RESUME', 1, (255, 255, 255)), (WIDTH // 2 - 120, HEIGHT // 2 - 200))
+    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 190, HEIGHT // 2 - 25, 400, 100))
+    WIN.blit(TEXT_FONT.render('MAIN MENU', 1, (255, 255, 255)), (WIDTH // 2 - 150, HEIGHT // 2))
+    pygame.display.update()
+
+def draw_win_window():
+    WIN.fill(BACKGROUND)
+    WIN.blit(TEXT_FONT.render('YOU WIN', 1, (255, 255, 255)), (WIDTH // 2 - 120, 50))
+    pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 190, HEIGHT // 2 - 225, 400, 100))
+    WIN.blit(TEXT_FONT.render('RESTART', 1, (255, 255, 255)), (WIDTH // 2 - 120, HEIGHT // 2 - 200))
     pygame.draw.rect(WIN, (255, 0, 0), pygame.Rect(WIDTH // 2 - 190, HEIGHT // 2 - 25, 400, 100))
     WIN.blit(TEXT_FONT.render('MAIN MENU', 1, (255, 255, 255)), (WIDTH // 2 - 150, HEIGHT // 2))
     pygame.display.update()
@@ -69,6 +99,8 @@ def main():
     state = 'menu'
     run = True
     while run:
+
+        #shows menu screen until start button is pressed
         if state == 'menu':
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
@@ -78,10 +110,11 @@ def main():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if WIDTH // 2 - 100 <= mouse[0] <= WIDTH // 2 + 100 and HEIGHT // 2 - 50 <= mouse[1] <= HEIGHT // 2 + 50:
                         state = 'play'
-                        cube.shuffle()
+                        #cube.scramble()
             mouse = pygame.mouse.get_pos()
             draw_menu_window()
 
+        #screen with cube that player can rotate with arrows
         elif state == "play":
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
@@ -89,36 +122,19 @@ def main():
                     pygame.quit()
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if 229 <= mouse[0] <= 229 + 45 and 13 <= mouse[1] <= 13 + 45:
-                        cube.turn("LU")
-                    elif 329 <= mouse[0] <= 329 + 45 and 13 <= mouse[1] <= 13 + 45:
-                        cube.turn("RU") 
-                    elif 229 <= mouse[0] <= 229 + 45 and 690 <= mouse[1] <= 690 + 45:
-                        cube.turn("LD")
-                    elif 329 <= mouse[0] <= 329 + 45 and 690 <= mouse[1] <= 690 + 45:
-                        cube.turn("RD")
-                    elif 19 <= mouse[0] <= 19 + 45 and 225 <= mouse[1] <= 225 + 45:
-                        cube.turn("BL")
-                    elif 19 <= mouse[0] <= 19 + 45 and 325 <= mouse[1] <= 325 + 45:
-                        cube.turn("FL")
-                    elif 539 <= mouse[0] <= 539 + 45 and 225 <= mouse[1] <= 225 + 45:
-                        cube.turn("BR")
-                    elif 539 <= mouse[0] <= 539 + 45 and 325 <= mouse[1] <= 325 + 45:
-                        cube.turn("FR")
-                    elif 384 <= mouse[0] <= 384 + 45 and 170 <= mouse[1] <= 170 + 45:
-                        cube.turn("UL")
-                    elif 174 <= mouse[0] <= 174 + 45 and 170 <= mouse[1] <= 170 + 45:
-                        cube.turn("UR")
-                    elif 384 <= mouse[0] <= 384 + 45 and 480 <= mouse[1] <= 480 + 45:
-                        cube.turn("DR")
-                    elif 174 <= mouse[0] <= 174 + 45 and 480 <= mouse[1] <= 480 + 45:
-                        cube.turn("DL")
-                    elif WIDTH - 130 <= mouse[0] <= WIDTH - 20 and 20 <= mouse[1] <= 70:
+                    for item in buttons:
+                        if item[0] <= mouse[0] <= item[0] + ARROW_BUTTON_SIZE[0] and item[1] <= mouse[1] <= item[1] + ARROW_BUTTON_SIZE[1]:
+                            cube.turn(buttons[item])
+                    if WIDTH - 130 <= mouse[0] <= WIDTH - 20 and 20 <= mouse[1] <= 70:
                         state = "pause"
             mouse = pygame.mouse.get_pos()
+
+            if cube.is_solved():
+                state = "win"
                         
             draw_game_window()
 
+        #pause screen has resume and main menu buttons, to either return to game or go to menu screen
         elif state == 'pause':
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
@@ -133,6 +149,23 @@ def main():
             mouse = pygame.mouse.get_pos()
 
             draw_pause_window()
+
+        #win screen when player completes cube
+        elif state == 'win':
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: 
+                    run = False
+                    pygame.quit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if WIDTH // 2 - 190 <= mouse[0] <= WIDTH // 2 + 210 and HEIGHT // 2 - 225 <= mouse[1] <= HEIGHT // 2 - 125:
+                        cube.scramble()
+                        state = 'play'
+                    elif WIDTH // 2 - 190 <= mouse[0] <= WIDTH // 2 + 210 and HEIGHT // 2 - 25 <= mouse[1] <= HEIGHT // 2 + 75:
+                        state = 'menu'
+            mouse = pygame.mouse.get_pos()
+
+            draw_win_window()
 
 
 if __name__ == "__main__":
